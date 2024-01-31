4ca978b7895e748a1416b2fb1b830ee3a83af7dc
FelipePassarela <felipepassarela8@hotmail.com>
2024-01-31 01:10:38 -0300

    Commented out debug output in Game::draw() function

--- source/game.cpp
+++ source/game.cpp
@@ -92,7 +92,7 @@ void Game::draw()
     #ifdef DEBUG
     std::cout << std::fixed << std::setprecision(2); 
     std::cout << "X=" << player.getX() << " Y=" << player.getY() << " A=" << player.getAngle() / PI << "pi" << std::endl;
-    std::cout << "RayDist: " << player.getRay().getDistance() << std::endl;
+    // std::cout << "RayDist: " << player.getRay().getDistance() << std::endl;
     #endif
 }
 
