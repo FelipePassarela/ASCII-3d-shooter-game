5b8d832e5009d0e321fdb32b26a59daccf23f13c
FelipePassarela <felipepassarela8@hotmail.com>
2024-02-03 10:04:43 -0300

    Remove done TODO

--- source/main.cpp
+++ source/main.cpp
@@ -1,5 +1,4 @@
 // TODO: Document all the code
-// TODO: Add 3d rendered scene (cast rays for each column of the screen)
 // REMINDER: Alt + O to go to header/source file
 // REMINDER: There is a option to create a definition/declaration of a function in menu
 
