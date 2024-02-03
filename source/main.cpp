a0254317f4bb3158ac22bb5798d0f6d3c27642e6
FelipePassarela <felipepassarela8@hotmail.com>
2024-02-03 13:07:03 -0300

    Remove reminder documentation

--- source/main.cpp
+++ source/main.cpp
@@ -1,6 +1,4 @@
 // TODO: Document all the code
-// REMINDER: Alt + O to go to header/source file
-// REMINDER: There is a option to create a definition/declaration of a function in menu
 
 #include <iostream>
 #include "game.hpp"
